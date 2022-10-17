f92b3867a05db58d75cb908e761d8f8be65998a6
Юлия Пшеничная <yulenykaa@gmail.com>
2022-10-17 17:17:11 +0300

    add-01

---
+++ js/01-gallery.js
@@ -0,0 +1,47 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+// Creation gallery
+const gallery = document.querySelector(".gallery");
+const imageGallery = createGallery(galleryItems);
+
+gallery.insertAdjacentHTML("afterbegin", imageGallery);
+gallery.addEventListener("click", handleClickOnImage);
+
+function createGallery(markup) {
+  return markup
+    .map((elem) => {
+      const { preview, original, description } = elem;
+      return `
+    <div class="gallery__item">
+     <a class="gallery__link" href="${original}">
+        <img 
+        class="gallery__image"
+        src=${preview}
+        data-source=${original}
+        alt=${description}/>
+     </a>
+        </div>`;
+    })
+    .join("");
+}
+
+//Click on image handler
+function handleClickOnImage(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}", width = "800", height = "600"></div>`
+  );
+  instance.show();
+
+    gallery.addEventListener("keydown", (event) => {
+      if (event.key === "Escape") {
+        instance.close();
+      }
+    });
+}
